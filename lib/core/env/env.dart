9942f2662dc34e99bc83253e2e5fe6a5c9c2b46f
felipe bellini <felipelemosbellini@gmail.com>
2025-12-29 20:32:45 -0300

    chore: changed .env name

--- lib/core/env/env.dart
+++ lib/core/env/env.dart
@@ -2,7 +2,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 abstract class Env {
   //base url
-  static String get pokedexBaseUrl => dotenv.env["PokemonGo-Pokedex"] ?? '';
+  static String get pokedexBaseUrl => dotenv.env["POKEMON_GO_POKEDEX"] ?? '';
 
   static Future<void> start() async {
     await dotenv.load(fileName: "./pokedex.env");
