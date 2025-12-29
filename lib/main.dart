0ce5a7355332c86a8014198551c72f220e8b8a96
felipe bellini <felipelemosbellini@gmail.com>
2025-12-29 20:32:16 -0300

    chore: changed .env name and start on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:pokedex/app_module.dart';
 import 'package:pokedex/app_widget.dart';
+import 'package:pokedex/core/env/env.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Env.start();
   runApp(ModularApp(module: AppModule(), child: const AppWidget()));
 }
