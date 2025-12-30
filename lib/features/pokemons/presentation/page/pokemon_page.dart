e1cd82fd256041583fce5b55434765da59f9afa6
felipe bellini <felipelemosbellini@gmail.com>
2025-12-30 02:34:08 -0300

    feat: created box_pokemon_widget.dart

--- lib/features/pokemons/presentation/page/pokemon_page.dart
+++ lib/features/pokemons/presentation/page/pokemon_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:pokedex/core/theme/app_colors.dart';
 import 'package:pokedex/features/pokemons/presentation/bloc/pokemon_bloc.dart';
+import 'package:pokedex/features/pokemons/presentation/widgets/app_bar/red_app_bar_pokeball.dart';
+import 'package:pokedex/features/pokemons/presentation/widgets/box_pokemon_widget.dart';
 
 class PokemonPage extends StatefulWidget {
   const PokemonPage({super.key});
@@ -23,20 +26,36 @@ class _PokemonPageState extends State<PokemonPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("start", style: TextStyle(color: Colors.black)),
-      ),
+      backgroundColor: Colors.white,
+      appBar: AppBar(backgroundColor: AppColors.red, toolbarHeight: 0),
       body: BlocBuilder(
         bloc: bloc,
         builder: <PokemonBloc, PokemonState>(context, state) {
-          if (state is PokemonLoaded)
-            return ListView.builder(
-              itemCount: state.pokemons.length,
-              itemBuilder: (_, index) {
-                return Text(state.pokemons[index].name);
-              },
+          if (state is PokemonLoaded) {
+            return ListView(
+              physics: AlwaysScrollableScrollPhysics(),
+              shrinkWrap: true,
+              children: [
+                RedAppBarPokeball(),
+                SizedBox(height: 40),
+                GridView.builder(
+                  shrinkWrap: true,
+                  padding: EdgeInsets.all(16),
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: state.pokemons.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 16,
+                    mainAxisSpacing: 16,
+                    childAspectRatio: 0.857,
+                  ),
+                  itemBuilder: (_, index) {
+                    return BoxPokemonWidget(pokemon: state.pokemons[index]);
+                  },
+                ),
+              ],
             );
-          else if (state is PokemonLoading)
+          } else if (state is PokemonLoading)
             return Center(child: Text("Error"));
           else if (state is PokemonError)
             return Center(child: Text("Error"));
---
+++ lib/features/pokemons/presentation/widgets/box_pokemon_widget.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex/core/theme/app_colors.dart';
+import 'package:pokedex/features/pokemons/data/models/pokemon.dart';
+
+class BoxPokemonWidget extends StatelessWidget {
+  final Pokemon pokemon;
+
+  const BoxPokemonWidget({super.key, required this.pokemon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        border: Border.all(color: AppColors.borderLowest.withOpacity(0.15)),
+      ),
+      padding: EdgeInsets.all(12),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Image.network(pokemon.img, height: 86),
+          ),
+          SizedBox(height: 8),
+          Text(pokemon.name),
+          SizedBox(height: 4),
+          Text("#" + pokemon.number),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Icon(Icons.add_circle, size: 16),
+          ),
+        ],
+      ),
+    );
+  }
+}
