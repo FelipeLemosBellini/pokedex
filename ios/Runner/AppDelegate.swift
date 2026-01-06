1bd3740e485c1bf78d5a401356de78f086ab1e3f
felipe bellini <felipelemosbellini@gmail.com>
2026-01-06 00:08:05 -0300

    refactor: came back with AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,56 +1,44 @@
 import UIKit
 import Flutter
+import FirebaseCore
+import FirebaseAnalytics
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
+    private let channelName = "com.pokedex"
+
     override func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
+
         GeneratedPluginRegistrant.register(with: self)
+
+        if FirebaseApp.app() == nil {
+            FirebaseApp.configure()
+        }
+
+        let controller = window?.rootViewController as! FlutterViewController
+        let analyticsChannel = FlutterMethodChannel(
+            name: channelName,
+            binaryMessenger: controller.binaryMessenger
+        )
+
+        analyticsChannel.setMethodCallHandler { (call: FlutterMethodCall, result: @escaping FlutterResult) in
+            switch call.method {
+            case "logEvent":
+                if let args = call.arguments as? [String: Any],
+                   let name = args["name"] as? String {
+                    let params = args["params"] as? [String: Any] ?? [:]
+                    Analytics.logEvent(name, parameters: params)
+                }
+                result(nil)
+
+            default:
+                result(FlutterMethodNotImplemented)
+            }
+        }
+
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
 }
-
-// import UIKit
-// import Flutter
-// import FirebaseCore
-// import FirebaseAnalytics
-//
-// @main
-// @objc class AppDelegate: FlutterAppDelegate {
-//     private let channelName = "com.pokedex"
-//
-//     override func application(
-//         _ application: UIApplication,
-//         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-//     ) -> Bool {
-//         let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
-//         let analyticsChannel = FlutterMethodChannel(
-//             name: channelName,
-//             binaryMessenger: controller.binaryMessenger
-//         )
-//
-//         analyticsChannel.setMethodCallHandler { [weak self] (call: FlutterMethodCall, result: @escaping FlutterResult) in
-//             guard let self = self else {
-//                 return
-//             }
-//
-//             switch call.method {
-//             case "logEvent":
-//                 if let args = call.arguments as? [String: Any],
-//                    let name = args["name"] as? String {
-//                     let params = args["params"] as? [String: Any] ?? [:]
-//                     Analytics.logEvent(name, parameters: params)
-//                 }
-//
-//                 result(nil)
-//
-//             default:
-//                 result(FlutterMethodNotImplemented)
-//             }
-//         }
-//
-//         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-//     }
-// }
